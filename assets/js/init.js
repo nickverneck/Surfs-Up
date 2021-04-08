01570bd5645561a907c7de0ec038f43ff181f467
Rob <r.jacobs790@gmail.com>
2021-04-08 14:41:58 -0400

    created funtion to store data and apphend to page

---
+++ assets/js/init.js
@@ -0,0 +1,116 @@
+var userLat;
+var userLon;
+var userCity;
+var cityID;
+var beachName;
+var beachID;
+
+$(document).ready(function() {
+  
+    // Gets the coordinates of the user's latitude, longitude, and city then stores them as variables.
+    function getLocation() {
+
+        var requestURL = "https://services.surfline.com/geo-target/region";
+
+        fetch(requestURL)
+        .then(function(response) {
+           
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            userLat = data.location.latitude;
+            userLon = data.location.longitude;
+            userCity = data.city.name;
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // Gets the current city id and stores it as a variable for later use.
+            var requestURL2 = "https://services.surfline.com/kbyg/mapview/spot?lat=" + userLat + "&lon=" + userLon + "";
+
+            fetch(requestURL2)
+            .then(function(response) {
+                
+                return response.json();
+            })
+            .then(function (data2) {
+                console.log(data2);
+                beachID = data2.spot._id;
+                beachName= data2.spot.name;
+                 fetchReport(beachID);
+                fetchNearby(beachID);
+            });
+        });
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // Test function to check that variables are stored. Automatically called at the end of getLocation()
+    function print(){
+        console.log(userLat);
+        console.log(userLon);
+        console.log(userCity);
+        console.log(cityID);
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    getLocation();
+
+    // Creates a map based on the user's current location
+    // let map;
+
+    // function initMap() {
+    // map = new google.maps.Map(document.getElementById("map"), {
+    //     center: { lat: userLat, lng: userLon },
+    //     zoom: 12,
+    // });
+    // }
+// close modal with mapp
+$('.close-modal').on("click", function(){
+  $('.modal').removeClass("is-active");
+
+});
+// 
+
+
+    $('.beachInfoDiv').on("click","#location-btn", initMap);
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Creates a map centered on the user's location then draws a connecting line to the beach in question.
+    // Directions are printed in a panel on the right side
+    var directionsService;
+    var directionsRenderer;
+    var endPoint;
+
+    function initMap() {
+    
+      $('.modal').attr("class","modal is-active");
+        directionsService = new google.maps.DirectionsService();
+        directionsRenderer = new google.maps.DirectionsRenderer();
+        userLoc = new google.maps.LatLng(userLat, userLon);
+        endPoint = new google.maps.LatLng(beachLat, beachLong);
+        var mapOptions = {
+          zoom: 12,
+          center: userLoc
+        }
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        directionsRenderer.setMap(map);
+        directionsRenderer.setPanel(document.getElementById('directionsPanel'));
+        calcRoute();
+      }
+      
+      function calcRoute() {
+        var request = {
+          origin: userLoc,
+          destination: beachName,
+          travelMode: 'DRIVING'
+        };
+        directionsService.route(request, function(result, status) {
+          if (status == 'OK') {
+            directionsRenderer.setDirections(result);
+          }
+         
+        });
+    }  
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+})
---
+++ assets/js/surf.js
@@ -0,0 +1,112 @@
+
+// let's create a object where holds all the urls needed for the API calls
+const surfline = {
+    url: "https://services.surfline.com",
+    searchUrl: "https://services.surfline.com/search/site?q=",
+    forecastWave: "https://services.surfline.com/kbyg/spots/forecasts/wave?spotId=",
+    forecastWind: "https://services.surfline.com/kbyg/spots/forecasts/wind?spotId=",
+    forecastWeather: "https://services.surfline.com/kbyg/spots/forecasts/weather?spotId=",
+    forecastTide: "https://services.surfline.com/kbyg/spots/forecasts/tides?spotId=",
+    report: "https://services.surfline.com/kbyg/spots/reports?spotId=",
+    nearby: "https://services.surfline.com/kbyg/spots/nearby?spotId=",
+    closest: " https://services.surfline.com/kbyg/mapview/spot?" //needs lat & lon
+
+}
+var beachLat ;
+    var beachLong ;
+// lets create a function that will grab the Surf Report data from surfline
+function fetchReport(id) {
+    fetch(surfline.report + id)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            renderReport(data);
+        })
+}
+// this function will render the report values into the main page
+// 
+function renderReport(data) {
+   console.log(data)
+    var currentTideT = data.forecast.tide.type;// low, normal or high tide
+    var currentTideH = data.forecast.tide.height + ' FT';
+    var waterTemp = data.forecast.waterTemp.min + '-' + data.forecast.waterTemp.max + ' F';
+    var weatherTemp = data.forecast.weather.temperature+" F";
+    var weatherCond = data.forecast.weather.condition;// returns clear, rainy, thunderstorm etc
+    var surfCond = data.forecast.conditions.value;  //returns poor(aka not worth going)poor to fair(maybe worth looking at) fair(you should check) good(dude go to the beach)or EPIC (make an excuse at work and wake up 5am to go surfing allday)
+    var waveType = data.forecast.waveHeight.humanRelation; // returns waist high, shoulder high , head high or overhead when sh* is going down.
+    var waveHeight = data.forecast.waveHeight.min + '-' + data.forecast.waveHeight.max + ' FT';
+    var windSpeed = data.forecast.wind.speed+" Knots";
+    var windDir = data.forecast.wind.direction;
+    var beachName = data.spot.name;
+   beachLat = data.spot.lat;
+     beachLong = data.spot.long;
+     var html = "<h1 class='title is-1'>"+beachName+"</h1><button id='location-btn' class='button is-primary'>Get Directions</button>"+
+     '<button class="button is-warning" id="favorites">Add to Favorites</button>'+
+     "<h3 class='title is-5'>Wave Height: "+waveHeight+"</h3>"+
+     "<h3 class='title is-5'>Wind Speed: "+windSpeed+"</h3>"+
+     "<h3 class='title is-5'>Weather Temperature: "+weatherTemp+"</h3>"+
+     "<h3 class='title is-5'>Water Temperature: "+waterTemp+"</h3>"+
+     "<h2 class='title is-4'>Surf Condition: " + surfCond + " "+waveType+"</h2>";
+     $(".beachInfoDiv").html(html);
+}
+
+// this one is to fetch data from nearby spots from a certain ID
+ function fetchNearby(id) {
+    fetch(surfline.nearby + id)
+        .then( function (response) {
+            return  response.json();
+        })
+        .then( function (api) {
+          renderNearby(api);
+          
+        })
+};
+
+// lets render the data fetched nearbyspots.
+// we create a list element
+
+// once we get the data we will limit the length otherwise too many options will appear
+// but if the lengh is less than the value we set just use the length
+// then with the data we populate the list element
+function renderNearby(api) {
+ 
+
+    var length;
+    if (api.data.spots.length >= 5) {
+        length = 5;
+    }
+    else {
+        length = api.data.spots.length
+    }
+    var h2El = document.createElement("h2")
+    $(h2El).text("Nearby Beaches");
+    $(h2El).attr("class","title is-4")
+    var ulEl = document.createElement("Ul")
+    $(ulEl).attr("class","menu-list")
+    console.log(h2El)
+    for (i = 0; i < length; i++) {
+        var spotName = api.data.spots[i].name;
+        var liEl = document.createElement("li")
+        $(liEl).text(spotName);
+        ulEl.append(liEl);
+
+    }
+    $(".nearbyDiv").html("");
+    $(".nearbyDiv").append(h2El);
+    $(".nearbyDiv").append(ulEl);
+    
+
+}
+
+ 
+function fetchClosest(id) {
+    return fetch(surfline.closest + id)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            return data;
+        })
+}
